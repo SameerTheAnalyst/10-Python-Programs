9374ac012032f5f37d261cf3b6dbaa1065cb9fdc
Sameer <engg.sameerawan@gmail.com>
2022-07-12 14:41:56 +0300

    9 - Number Guessing Game ( Better )

--- blackjack game.py
+++ blackjack game.py
@@ -1,70 +1,52 @@
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 import random
-
-def deal_card():
-  """Returns a random card from the deck."""
-  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-  card = random.choice(cards)
-  return card
-
-def calculate_score(cards):
-  """Take a list of cards and return the score calculated from the cards"""
-
-  if sum(cards) == 21 and len(cards) == 2:
-    return 0
-  if 11 in cards and sum(cards) > 21:
-    cards.remove(11)
-    cards.append(1)
-  return sum(cards)
-
-def compare(user_score, computer_score):
-  if user_score == computer_score:
-    return "\nDraw "
-  elif computer_score == 0:
-    return "\nLose, opponent has Blackjack "
-  elif user_score == 0:
-    return "\nWin with a Blackjack "
-  elif user_score > 21:
-    return "\nYou went over. You lose "
-  elif computer_score > 21:
-    return "\nOpponent went over. You win "
-  elif user_score > computer_score:
-    return "\nYou win "
-  else:
-    return "\nYou lose "
-
-def play_game():
-  user_cards = []
-  computer_cards = []
-  is_game_over = False
-
-  for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
-  while not is_game_over:
-    user_score = calculate_score(user_cards)
-    computer_score = calculate_score(computer_cards)
-    print(f"\n   Your cards: {user_cards}, current score: {user_score}")
-    print(f"\n   Computer's first card: {computer_cards[0]}")
-
-    if user_score == 0 or computer_score == 0 or user_score > 21:
-      is_game_over = True
+num = random.randint(1,100)
+guess = []
+guess_count = 0
+out_of_guesses = False
+print("Welcome to the Number Guessing Game!")
+print("\nI am thinking of a number between 1 and 100, can you guess it?")
+difficulty = input("\nChoose a difficulty, Type 'easy' or 'hard': ")
+if difficulty.lower() == "easy":
+    guess_limit = 10
+    print("\nYou have 10 attempts to guess the number between 1 and 100")
+    while guess != num and not (out_of_guesses):
+        if guess_count < guess_limit:
+            guess = int(input("Enter your guess: "))
+            guess_count += 1
+            if guess > num + 10:
+                print("Your number is too high\n")
+            elif guess > num:
+                print("Your number is high\n")
+            elif guess < num - 10:
+                print("Your number is too low\n")
+            elif guess < num:
+                print("Your number is low\n")
+        else:
+            out_of_guesses = True
+    if out_of_guesses:
+        print(f"Sorry, your are out of guesses, the number was {num}")
     else:
-      user_should_deal = input("\nType 'y' to get another card, type 'n' to pass: ")
-      if user_should_deal == "y":
-        user_cards.append(deal_card())
-      else:
-        is_game_over = True
-
-  while computer_score != 0 and computer_score < 17:
-    computer_cards.append(deal_card())
-    computer_score = calculate_score(computer_cards)
-
-  print(f"\n   Your final hand: {user_cards}, final score: {user_score}")
-  print(f"\n   Computer's final hand: {computer_cards}, final score: {computer_score}")
-  print(compare(user_score, computer_score))
-
-while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
-  play_game()
+        print("Congratulations, You WON!!!")
+elif difficulty.lower() == "hard":
+    guess_limit = 5
+    print("\nYou have 5 attempts to guess the number between 1 and 100")
+    while guess != num and not (out_of_guesses):
+        if guess_count < guess_limit:
+            guess = int(input("Enter your guess: "))
+            guess_count += 1
+            if guess > num + 10:
+                print("Your number is too high\n")
+            elif guess > num:
+                print("Your number is high\n")
+            elif guess < num - 10:
+                print("Your number is too low\n")
+            elif guess < num:
+                print("Your number is low\n")
+        else:
+            out_of_guesses = True
+    if out_of_guesses:
+        print(f"Sorry, your are out of guesses, the number was {num}.")
+    else:
+        print("Congratulations, You WON!!!")
+else:
+    print("Invalid Command")
